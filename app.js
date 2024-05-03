d5bd26260c4b5c25791869c8c112be02975aa56d
AlbertCastellon <albertkstyon@gmail.com>
2024-05-03 20:42:04 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const info = {success: true,
+	 data: 'dos'};
+
 app.get('/api/health', (req, res) => {
 	// Your code here
+	res.json(info)
 })
 
 app.listen(port, () =>
